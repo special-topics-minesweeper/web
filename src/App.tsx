e846a4ee61e934d170fa8f9c246401f0aae75ec1
lilithkarapetyan <lilith.karapetyann@gmail.com>
2021-05-04 21:38:34 +0400

    add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,26 @@ import Board from "./containers/Board";
 import Sidebar from "./containers/Sidebar";
 import useStyles from "./styles";
 
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
 function App() {
   const classes = useStyles();
   return (
-    <Header>
-      <div className={classes.root}>
-        <Board size={12}/>
-        <Sidebar />
-      </div>
-    </Header>
-
+    <ThemeProvider theme={theme}>
+      <Header>
+        <div className={classes.root}>
+          <Board size={12}/>
+          <Sidebar />
+        </div>
+      </Header>
+    </ThemeProvider>
   );
 }
 
