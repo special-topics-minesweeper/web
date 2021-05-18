417d55b4b71d63a03d5e5cc8d35425739597949d
lilithkarapetyan <lilith.karapetyann@gmail.com>
2021-05-18 22:51:40 +0400

    removed logs

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,2 @@
 export const DRAWER_WIDTH = 240;
+export const API_BASE_URL = 'https://api.minesweepers.games/';
--- src/utils/fetch/index.ts
+++ src/utils/fetch/index.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import { get as getToken } from "../token";
+import { API_BASE_URL } from "../../config/constants";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: API_BASE_URL,
   timeout: 10000,
 })
 
-console.log('process.env', process.env);
 instance.interceptors.request.use((config) => {
   config.headers.key = getToken();
   return config;
