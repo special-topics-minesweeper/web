5de663ace7d00dfd19a1f32fff8f183f2b496506
lilithkarapetyan <lilith.karapetyann@gmail.com>
2021-05-19 08:50:48 +0400

    fixed profile age

--- src/containers/Profile/index.tsx
+++ src/containers/Profile/index.tsx
@@ -1,42 +1,54 @@
-import useStyles from "./styles";
+import { useContext, useEffect, useState } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import { Avatar } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import LinearProgress from "@material-ui/core/LinearProgress";
-import { useEffect, useState } from "react";
+import { useHistory } from 'react-router-dom';
 import classNames from "classnames";
+import useStyles from "./styles";
+import { UserContext } from "../../utils/contexts";
 
 const Profile = () => {
   const classes = useStyles();
+  const { user } = useContext(UserContext);
+  const history = useHistory();
   const [percent, setPercent] = useState(0);
+
   useEffect(() => {
-    setPercent(70);
-  }, []);
+    if(!user) return;
+    setPercent(user.wins_count / user.games_count * 100 || 0);
+  }, [user]);
+
+  if(!user) {
+    history.replace('play');
+    return null;
+  }
+
   return (
     <Container component="main">
       <CssBaseline/>
       <Card className={classes.root}>
         <div className={classes.user}>
-        <Avatar aria-label="recipe" className={classes.avatar}>
-          FL
-        </Avatar>
-        <Typography className={classes.name}>
-          <span>Firstname Lastname</span>
-          <br/><
-          span className={classes.username}>username</span>
-        </Typography>
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            {`${user.firstname[0]}${user.lastname[0]}`}
+          </Avatar>
+          <Typography className={classes.name}>
+            <span>{user.firstname} {user.lastname}</span>
+            <br/>
+            <span className={classes.username}>{user.username}</span>
+          </Typography>
         </div>
         <div className={classes.progressContainer}>
-          <Typography className={classes.played}>5 wins out 8</Typography>
+          <Typography className={classes.played}>{user.wins_count} wins out {user.games_count}</Typography>
           <LinearProgress variant="determinate" value={percent} className={classes.progress}/>
         </div>
         <Typography className={classes.title}> Best Times </Typography>
         <div className={classes.scoresContainer}>
           <div className={classes.scoreCard}>
             <div className={classNames(classes.scoreContent, 'easy')}>
-              <h1 className={classes.score}>112</h1>
+              <h1 className={classes.score}>{ user.best_results.easy >= 0 ? user.best_results.easy : '-' }</h1>
               <div className={classes.seconds}>seconds</div>
               <div className={classes.scoreDivider}/>
               <div className={classes.difficulty}>Easy</div>
@@ -45,7 +57,7 @@ const Profile = () => {
           </div>
           <div className={classes.scoreCard}>
             <div className={classNames(classes.scoreContent, 'medium')}>
-              <h1 className={classes.score}>612</h1>
+              <h1 className={classes.score}>{user.best_results.medium >= 0 ? user.best_results.medium : '-'} </h1>
               <div className={classes.seconds}>seconds</div>
               <div className={classes.scoreDivider}/>
               <div className={classes.difficulty}>Medium</div>
@@ -54,7 +66,7 @@ const Profile = () => {
           </div>
           <div className={classes.scoreCard}>
             <div className={classNames(classes.scoreContent, 'hard')}>
-              <h1 className={classes.score}>182</h1>
+              <h1 className={classes.score}>{user.best_results.hard >= 0 ? user.best_results.hard : '-'}</h1>
               <div className={classes.seconds}>seconds</div>
               <div className={classes.scoreDivider}/>
               <div className={classes.difficulty}>Hard</div>
--- src/utils/contexts.ts
+++ src/utils/contexts.ts
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 
 export const UserContext = createContext({
-  user: null,
+  user: {} as any,
   setUser: (user: any) => { },
 });
